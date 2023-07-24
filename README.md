3bfc1a8a374c758d1e6b44d1dda4c1394d705c72
yanxiyue <wuling@cooder.org>
2023-07-24 19:45:46 +0800

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+<h1 align="center">Json Translator</h1>
+Translate the input of natural language into a specified structure.
+
+![](./public/snapshot.png)
+
+- powered by [microsoft/TypeChat](https://github.com/microsoft/TypeChat)
 
 ## Getting Started
 
@@ -14,21 +20,25 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
 ## Deploy on Vercel
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fcooder-org%2Fjson-translator&env=OPENAI_API_KEY&project-name=json-translator&repository-name=json-translator)
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+## API ENDPOINT
+
+ - /api/translate
+
+```bash
+curl -X 'POST' \
+  'http://127.0.0.1:3000/api/translate' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "schema": "// The following is a schema definition for determining whether a user wants to share a post or not:\nexport interface ShareOrNot {\nisShare: boolean;\nurl: string;\ncomment: string;\n}",
+  "typeName": "ShareOrNot",
+  "prompt": "https://github.com/shengxia/RWKV_Role_Playing_API 一个基于Flask实现的RWKV角色扮演API"
+}'
+
+```
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import { Audio } from "react-loader-spinner";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    typeName: "ShareOrNot",
+    schema:
+      "// The following is a schema definition for determining whether a user wants to share a post or not:\n\nexport interface ShareOrNot {\n  isShare: boolean;\n  url: string;\n  comment: string;\n}",
+    output: "",
+    prompt: "https://github.com/shengxia/RWKV_Role_Playing_API 一个基于Flask实现的RWKV角色扮演API",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onClick = async () => {
+    setLoading(true);
+    const response = await fetch("/api/translate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    setFormData((prevState) => ({ ...prevState, output: JSON.stringify(data, null, 2) }));
+    setLoading(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      {loading && (
+        <Audio
+          height="80"
+          width="80"
+          color="green"
+          ariaLabel="loading"
+          wrapperClass="fixed top-0 left-0 right-0 bottom-0 h-screen z-50 overflow-hidden bg-gray-100 opacity-75 block items-center justify-center"
         />
+      )}
+      <div className="sm:mx-auto sm:w-full">
+        <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          translate the input of natural language into a specified structure
+        </h2>
+        <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">将自然语言的输入提取为指定的结构</h2>
       </div>
+      <main className="flex flex-row items-center justify-between p-2">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-2 lg:px-8">
+          <div className="mt-1 sm:mx-auto sm:w-full">
+            <div className="space-y-6">
+              <div>
+                <label htmlFor="typeName" className="block text-sm font-medium leading-6 text-gray-900">
+                  Type Name（类型名称）
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="typeName"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formData.typeName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="schema" className="block text-sm font-medium leading-6 text-gray-900">
+                    Type Definition In TS （类型描述 Typescript）
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <CodeEditor
+                    name="schema"
+                    value={formData.schema || ""}
+                    language="typescript"
+                    placeholder="Please enter typescript."
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        schema: e.target.value,
+                      })
+                    }
+                    padding={15}
+                    style={{
+                      fontSize: 12,
+                      // backgroundColor: "#f5f5f5",
+                      fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                    }}
+                  />
+                </div>
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="prompt" className="block text-sm font-medium leading-6 text-gray-900">
+                    Input（输入）
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <textarea
+                    name="prompt"
+                    rows={3}
+                    className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formData.prompt}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+              <div>
+                <button
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={onClick}
+                  disabled={loading}
+                >
+                  {loading ? "Translating（转换中）" : "Translate (转换)"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div>
+            <label htmlFor="output" className="block text-sm font-medium leading-6 text-gray-900">
+              Output（输出）
+            </label>
+            <div className="mt-2">
+              <CodeEditor
+                name="output"
+                value={formData.output || ""}
+                language="json"
+                padding={15}
+                minHeight={300}
+                style={{
+                  fontSize: 12,
+                  // backgroundColor: "#f5f5f5",
+                  fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
