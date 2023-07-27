9d8831b1c037f4ce3e501179743de7b214b5d46e
yanxiyue <wuling@cooder.org>
2023-07-27 10:06:20 +0800

    feat: changge storage from googlesheets to mysql

--- app/api/endpoint/[id]/route.ts
+++ app/api/endpoint/[id]/route.ts
@@ -1,19 +1,37 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
 import { createLanguageModel, createJsonTranslator, processRequests } from "typechat";
-import { get, Endpoint } from "../../../../lib/googlesheets";
+import { executeQuery } from "@/lib/db";
 
 const model = createLanguageModel(process.env);
 
-export async function POST(req: NextRequest, context: { params: any }) {
-  const slug = context.params;
-  const {prompt} = await req.json();
-  const endpoint = await get(slug.id);
-  const { schema, typeName } = endpoint as Endpoint;
+export async function POST(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
+  const { prompt } = await req.json();
 
-  const translator = createJsonTranslator(model, schema, typeName);
+  if (!prompt) {
+    return NextResponse.json(
+      { error: "Missing required parameters: prompt" },
+      { status: 400, statusText: "Bad Request" }
+    );
+  }
+
+  const query = `SELECT * FROM endpoints WHERE type_id = ?`;
+  const values = [id];
+  const recs = await executeQuery({ query, values });
+  const record = (recs as any[])[0];
+
+  if (!record) {
+    return NextResponse.json(
+      { error: `Endpoint with id ${id} not found` },
+      { status: 404, statusText: "Not Found" }
+    );
+  }
+
+  const { type_name, type_schema } = record;
+  const translator = createJsonTranslator(model, type_schema, type_name);
   const resp = await translator.translate(prompt);
   const data = resp.success ? resp.data : { error: resp.message };
+
   return NextResponse.json(data);
 }
---
+++ lib/db.ts
@@ -0,0 +1,38 @@
+import mysql from "serverless-mysql";
+import mysql2 from "mysql2";
+
+export const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT!),
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    insecureAuth: true,
+  },
+  library: mysql2 as unknown as Function,
+});
+
+export interface QueryParams {
+  query: string;
+  values?: any[];
+}
+
+export interface DBError {
+  error: any;
+  code: string;
+  errno: number;
+  sqlMessage: string;
+  sqlState: string;
+  sql: string;
+}
+
+export async function executeQuery({ query, values }: QueryParams) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    return {error};
+  }
+}
